10e20802f6d22d2780551feaa8ce6adfd853af3c
Brian Stascavage <bstascav@mog.home>
2014-07-30 23:51:47 -0400

    Updated camping-server module to include log files

--- modules/camping/manifests/init.pp
+++ modules/camping/manifests/init.pp
@@ -3,16 +3,30 @@ class camping {
     file {'/etc/default/camping-server':
 	source => '/vagrant/puppet/modules/camping/files/camping-server.default',
 	mode => 755,
+	notify => Service['camping-server'],
     }
 
     file {'/etc/init.d/camping-server':
         source => '/vagrant/puppet/modules/camping/files/camping-server.init',
         mode => 755,
+	notify => Service['camping-server'],
+    }
+
+    file {'/var/log/camping-server':
+	ensure => 'directory',
+    }
+
+    file {'/var/log/camping-server/log':
+	ensure => 'file',
+	mode => 666,
+	require => File['/var/log/camping-server'],
+	notify => Service['camping-server']
     }
 
     file {'/app/groupmestats/camping/web.yaml':
 	source => '/vagrant/puppet/modules/camping/files/web.yaml',
 	mode => 744,
+	notify => Service['camping-server'],
     }
 
     package { 'camping':
@@ -22,9 +36,7 @@ class camping {
     service { 'camping-server':
 	ensure => running,
 	enable => true,
-	require => [ File['/etc/default/camping-server'],
-		     File['/etc/init.d/camping-server'],
-		     File['/app/groupmestats/camping/web.yaml'],
-		     Package['camping'] ],
+	require => [ Package['camping'],
+		     File['/etc/init.d/camping-server'] ],
     }
 }
